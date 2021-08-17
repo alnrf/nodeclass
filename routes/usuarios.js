acff7ce0389f149e01e0ccd01a6cdfbfdd8e5b21
Allan <allanferreira@betalearning.com.br>
2021-08-17 18:52:08 -0300

    JWT

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,25 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../database");
+const bcrypt = require("bcrypt-nodejs");
+const jwt = require("jsonwebtoken");
 
 //import the controllers
 const usuariosController = require("../controllers/usuarios-controller");
 
-router.get("/todos", usuariosController.getUsuarios);
+router.get("/getUsuarios", usuariosController.getUsuarios);
 
-router.post("/", async (req, res, next) => {
+router.post("/cadastro", async (req, res, next) => {
+  var hash = bcrypt.hashSync(req.body.senha);
+  console.log(hash);
   try {
-    const sqlQuery = "INSERT INTO usuarios (nome, senha) values (?,?)";
-    const rows = await pool.query(sqlQuery, [req.body.nome, req.body.senha]);
-    res.status(201).send({ mensagem: "Usuário criado com sucesso", log: rows });
+    const sqlQuery = "INSERT INTO usuarios (email, senha) values (?,?)";
+    const results = await pool.query(sqlQuery, [req.body.email, hash]);
+    res
+      .status(201)
+      .send({ mensagem: "Usuário criado com sucesso", log: results });
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
 
+router.post("/login", async (req, res, next) => {
+  try {
+    const sqlQuery = `SELECT * FROM usuarios WHERE email = ?`;
+    var results = await pool.query(sqlQuery, [req.body.email]);
+    res.status(200).json(results[0].senha);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
 router.get("/:id", async (req, res, next) => {
   try {
-    const sqlQuery = "SELECT * from usuarios where id=?";
+    const sqlQuery = "SELECT * from usuarios where userId=?";
     const rows = await pool.query(sqlQuery, req.params.id);
     res.status(200).json(rows);
   } catch (error) {
